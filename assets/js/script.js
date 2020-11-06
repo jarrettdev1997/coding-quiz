d19458f1233deef501111b9ee924e251d9246a0c
Jarrett Devose <jarrett.dev1997@gmail.com>
2020-11-06 15:45:04 -0500

    start button functional

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Global Variables
+var quizContentHandler = document.querySelector("#start-quiz");
+var quizQuestions = [];
+
+// Create fxns that handle quiz interaction
+var startQuizHandler = function(event) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+// Event Listeners
+quizContentHandler.addEventListener("click", startQuizHandler);
